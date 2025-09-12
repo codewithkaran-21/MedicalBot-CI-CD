7bb17f6a081454fa57f47a321d161dc28c5f29f0
Karan Singh <godthatback21@gmail.com>
2025-09-12 13:15:40 +0530

    experiment notbook done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages , setup 
+
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author= "Karan Singh",
+    author_email="karan123@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
